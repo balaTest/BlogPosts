4998efcbeb49a04a7c8b0a23f2565b0c01045784
balaTest <balaTest@live.com>
2013-12-18 13:05:05 -0800

    Remap framework scripts

--- WebFormsNoAuth/WebFormsNoAuth/App_Start/BundleConfig.cs
+++ WebFormsNoAuth/WebFormsNoAuth/App_Start/BundleConfig.cs
@@ -10,6 +10,23 @@ namespace WebFormsNoAuth
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+                            "~/Scripts/WebForms/WebForms.js",
+                            "~/Scripts/WebForms/WebUIValidation.js",
+                            "~/Scripts/WebForms/MenuStandards.js",
+                            "~/Scripts/WebForms/Focus.js",
+                            "~/Scripts/WebForms/GridView.js",
+                            "~/Scripts/WebForms/DetailsView.js",
+                            "~/Scripts/WebForms/TreeView.js",
+                            "~/Scripts/WebForms/WebParts.js"));
+
+            // Order is very important for these files to work, they have explicit dependencies
+            bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
+
             BundleTable.EnableOptimizations = true;
         }
     }
