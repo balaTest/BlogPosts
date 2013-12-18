4eda05c09524b585c6f74ddfb421b5d148e5cc9f
balaTest <balaTest@live.com>
2013-12-18 06:11:40 -0800

    Enabling bundling and minification of css files

---
+++ WebFormsNoAuth/WebFormsNoAuth/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebFormsNoAuth
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- WebFormsNoAuth/WebFormsNoAuth/Global.asax.cs
+++ WebFormsNoAuth/WebFormsNoAuth/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -14,6 +15,7 @@ namespace WebFormsNoAuth
         protected void Application_Start(object sender, EventArgs e)
         {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
     }
