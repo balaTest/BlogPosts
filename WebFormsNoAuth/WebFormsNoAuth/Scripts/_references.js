c4fa532ca358de3c4f74aaf508c5ce9f7a8269c1
balaTest <balaTest@live.com>
2013-12-18 17:49:20 -0800

    Added _references.js

---
+++ WebFormsNoAuth/WebFormsNoAuth/Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="jquery-2.0.3.js" />
+/// <reference path="modernizr-2.7.1.js" />
+/// <reference path="respond.js" />
